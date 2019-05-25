ca2acadc1db050997da5ee120750bf4ff66be8e3
Matt Wiggins <matt.wiggins@legworkstudio.com>
2019-05-25 12:51:13 -0600

    Vue/Nuxt setup.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# goodbye-legwork
-Hey, it was a good run. 😥
+# Goodbye Legwork
+
+> Hey, it was a good run!
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+    <h1 class="title">
+      Goodbye Legwork
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
